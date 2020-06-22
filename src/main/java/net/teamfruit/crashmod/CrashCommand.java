c11a8c563ea8f4d2fd7f5c63b213f2a9163c1c02
Kamesuta <kamesuta@gmail.com>
2020-06-23 01:34:21 +0900

    タブ補完

--- src/main/java/net/teamfruit/crashmod/CrashCommand.java
+++ src/main/java/net/teamfruit/crashmod/CrashCommand.java
@@ -8,10 +8,18 @@ import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.play.server.SPacketSpawnMob;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextFormatting;
+import org.apache.commons.lang3.StringUtils;
+import scala.actors.threadpool.Arrays;
 
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CrashCommand extends CommandBase {
     private final CrashMod mod;
@@ -49,6 +57,17 @@ public class CrashCommand extends CommandBase {
             sender.sendMessage(new TextComponentString(TextFormatting.DARK_GREEN + "[" + TextFormatting.DARK_GREEN + "★" + TextFormatting.DARK_GREEN + "] " + TextFormatting.GREEN + args[0] + "をクラッシュさせました"));
             server.getPlayerList().sendMessage(new TextComponentString(TextFormatting.DARK_PURPLE + "[★] " + TextFormatting.LIGHT_PURPLE + sender.getName() + "が" + TextFormatting.RED + args[0] + "をクラッシュさせました"));
         }
+    }
+
+    @Override public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
+        if (args.length == 1) {
+            String arg = args[0];
+            List<String> players = Arrays.asList(server.getOnlinePlayerNames());
+            if (arg.length() > 0)
+                players = players.stream().filter(e -> StringUtils.startsWithIgnoreCase(e, arg)).collect(Collectors.toList());
+            return players;
+        }
+        return Collections.emptyList();
     }
 
     private void crash(final EntityPlayerMP p) {
